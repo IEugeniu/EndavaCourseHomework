03fb73479b629ff22596cbe5afc39bc6052c116e
IEugeniu <ignateugen4@gmail.com>
2019-10-24 22:41:22 +0300

    Adaugarea homework-ului pentru tema JAVA Fundamentals

---
+++ JavaFundamentals/src/com/company/Driver.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Driver extends Man {
+    private String name;
+    private String driverLicense;
+
+    public String getDriverLicense() {
+        return driverLicense;
+    }
+
+    public void setDriverLicense(String driverLicense) {
+        this.driverLicense = driverLicense;
+    }
+}
